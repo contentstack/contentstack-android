e06bbc16ba34be79a92a53526111a50c2698f9a7
Vikram Kalta <vikramkalta@Vikrams-MacBook-Pro.local>
2024-05-08 22:45:43 +0100

    feat: added support to convert json to html

--- contentstack/src/main/java/com/contentstack/sdk/SDKUtil.java
+++ contentstack/src/main/java/com/contentstack/sdk/SDKUtil.java
@@ -8,7 +8,10 @@ import android.content.Intent;
 import android.os.SystemClock;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Attr;
 
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
@@ -19,9 +22,15 @@ import java.security.MessageDigest;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.TimeZone;
+import java.util.jar.Attributes;
+import java.util.stream.StreamSupport;
 
 /**
  * @author contentstack.com
@@ -246,4 +255,190 @@ public class SDKUtil {
         return cal;
     }
 
+    public static void jsonToHTML(JSONArray entryArray, String[] keyPath, Option option) throws JSONException {
+        for (int i = 0; i < entryArray.length(); i++) {
+            Object jsonObj = entryArray.get(i);
+            jsonToHTML((JSONObject) jsonObj, keyPath, option);
+        }
+    }
+
+    public static void jsonToHTML(JSONObject entry, String[] keyPath, Option renderOption) throws JSONException {
+        MetaToEmbedCallback converter = metadata -> {
+            boolean available = entry.has("_embedded_items");
+            if (available) {
+                JSONObject jsonArray = entry.optJSONObject("_embedded_items");
+                return findEmbeddedItems(jsonArray, metadata);
+            }
+            return Optional.empty();
+        };
+        ContentCallback callback = content -> {
+            if (content instanceof JSONArray) {
+                JSONArray contentArray = (JSONArray) content;
+                return enumerateContents(contentArray, renderOption, converter);
+            } else if (content instanceof JSONObject) {
+                JSONObject jsonObject = (JSONObject) content;
+                return enumerateContent(jsonObject, renderOption, converter);
+            }
+            return null;
+        };
+        for (String path: keyPath) {
+            findContent(entry, path, callback);
+        }
+    }
+
+    private static void findContent(JSONObject entryObj, String path, ContentCallback contentCallback) throws JSONException {
+        String[] arrayString = path.split("\\.");
+        getContent(arrayString, entryObj, contentCallback);
+    }
+
+    private static void getContent(String[] arrayString, JSONObject entryObj, ContentCallback contentCallback) throws JSONException {
+        if (arrayString != null && arrayString.length != 0) {
+            String path = arrayString[0];
+            if (arrayString.length == 1) {
+                Object varContent = entryObj.opt(path);
+                if (varContent instanceof String || varContent instanceof JSONArray || varContent instanceof JSONObject) {
+                    entryObj.put(path, contentCallback.contentObject(varContent));
+                }
+            } else {
+                List<String> list = new ArrayList<>(Arrays.asList(arrayString));
+                list.remove(path);
+                String[] newArrayString = list.toArray(new String[0]);
+                if (entryObj.opt(path) instanceof JSONObject) {
+                    getContent(newArrayString, entryObj.optJSONObject(path), contentCallback);
+                } else if (entryObj.opt(path) instanceof JSONArray) {
+                    JSONArray jsonArray = entryObj.optJSONArray(path);
+                    for (int idx = 0; idx < jsonArray.length(); idx++) {
+                        getContent(newArrayString, jsonArray.optJSONObject(idx), contentCallback);
+                    }
+                }
+            }
+        }
+    }
+
+    private static Optional<JSONObject> findEmbeddedItems(JSONObject jsonObject, Metadata metadata) {
+        Set<String> allKeys = (Set<String>) jsonObject.keys();
+        for (String key: allKeys) {
+            JSONArray jsonArray = jsonObject.optJSONArray(key);
+
+            Optional<JSONObject> filteredContent = Optional.empty();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                try {
+                    JSONObject obj = jsonArray.getJSONObject(i);
+                    if (jsonObject.optString("uid").equalsIgnoreCase(metadata.getItemUid())) {
+                        filteredContent = Optional.of(obj);
+                        break;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (filteredContent.isPresent()) {
+                return filteredContent;
+            }
+        }
+        return Optional.empty();
+    }
+
+    private static Object enumerateContents(JSONArray contentArray, Option renderObject, MetaToEmbedCallback item) {
+        JSONArray jsonArrayRTEContent = new JSONArray();
+        for (int i = 0; i < contentArray.length(); i++) {
+            Object RTE = null;
+            try {
+                RTE = contentArray.get(i);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+            JSONObject jsonObject = (JSONObject) RTE;
+            String renderContent = enumerateContent(jsonObject, renderObject, item);
+            jsonArrayRTEContent.put(renderContent);
+        }
+        return jsonArrayRTEContent;
+    }
+    private static String enumerateContent(JSONObject jsonObject, Option renderObject, MetaToEmbedCallback item) {
+        if (jsonObject.length() > 0 && jsonObject.has("type") && jsonObject.has("children")) {
+            if (jsonObject.opt("type").equals("doc")) {
+                return doRawProcessing(jsonObject.optJSONArray("children"), renderObject, item);
+            }
+        }
+        return "";
+    }
+    private static String doRawProcessing(JSONArray children, Option renderObject, MetaToEmbedCallback embedItem) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < children.length(); i++) {
+            Object item = null;
+            try {
+                item = children.get(i);
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+            JSONObject child;
+            if (item instanceof JSONObject) {
+                child = (JSONObject) item;
+                stringBuilder.append(extractKeys(child, renderObject, embedItem));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private static final String ASSET = "asset";
+    private static String extractKeys(JSONObject jsonNode, Option renderObject, MetaToEmbedCallback embedItem) {
+        if (!jsonNode.has("type") && jsonNode.has("text")) {
+            return NodeToHTML.textNodeToHTML(jsonNode, renderObject);
+        } else if (jsonNode.has("type")) {
+            String nodeType = jsonNode.optString("type");
+            if (nodeType.equalsIgnoreCase("reference")) {
+                JSONObject attrObj = jsonNode.optJSONObject("attrs");
+                String attrType = attrObj.optString("type");
+                Metadata metadata;
+
+                if (attrType.equalsIgnoreCase(ASSET)) {
+                    String text = attrObj.optString("text");
+                    String uid = attrObj.optString("asset-uid");
+                    String style = attrObj.optString("display-type");
+                    metadata = new Metadata(text, attrType, uid, ASSET, style, "", new Attributes());
+                } else {
+                    String text = attrObj.optString("text");
+                    String uid = attrObj.optString("entry-uid");
+                    String contentType = attrObj.optString("content-type-uid");
+                    String style = attrObj.optString("display-type");
+                    metadata = new Metadata(text, attrType, uid, contentType, style, "", new Attributes());
+                }
+                Optional<JSONObject> filteredContent = embedItem.toEmbed(metadata);
+                if (filteredContent.isPresent()) {
+                    JSONObject contentToPass = filteredContent.get();
+                    return getStringOption(renderObject, metadata, contentToPass);
+                } else {
+                    if (attrType.equalsIgnoreCase(ASSET)) {
+                        return renderObject.renderNode("img", jsonNode, nodeJsonArray -> doRawProcessing(nodeJsonArray, renderObject, embedItem));
+                    }
+                }
+            } else {
+                return renderObject.renderNode(nodeType, jsonNode, nodeJsonArray -> doRawProcessing(nodeJsonArray, renderObject, embedItem));
+            }
+        }
+        return "";
+    }
+    private static String getStringOption(Option option, Metadata metadata, JSONObject contentToPass) {
+        String stringOption = option.renderOptions(contentToPass, metadata);
+        if (stringOption == null) {
+            DefaultOption defaultOptions = new DefaultOption();
+            stringOption = defaultOptions.renderOptions(contentToPass, metadata);
+        }
+        return stringOption;
+    }
+}
+
+interface MetaToEmbedCallback {
+    Optional<JSONObject> toEmbed(Metadata metadata);
+}
+interface ContentCallback {
+
+    /**
+     * The function contentObject takes an object as input and returns an object as output.
+     *
+     * @param content The content parameter is an object that represents the content to be passed to
+     *                the contentObject function.
+     * @return The contentObject is being returned.
+     */
+    Object contentObject(Object content);
 }
